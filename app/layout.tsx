7ad5ae755a10d95f2330819caae2d3333e78ed17
Farell Elghifari P <farellelghifarip@Farells-MacBook-Pro.local>
2026-03-05 19:33:28 +0700

    update: latest update on fill information

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,11 +21,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/images/logo-light.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/images/logo-dark.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
